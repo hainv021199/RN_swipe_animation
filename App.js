49db10cee584a32cbf4acdcd2278e84fd18e210d
VHai dev <hai.nguyenviet021199@gmail.com>
2022-04-16 15:42:16 +0700

    done section 5

--- App.js
+++ App.js
@@ -6,56 +6,38 @@ const DATA = [
   {
     id: 1,
     text: "Card #1",
-    uri: "https://www.bestofbirthdays.com/images-with-name/create-happy-birthday-urishilla-card-online-free-4",
+    source: require("./assets/pic1.jpg"),
   },
   {
     id: 2,
     text: "Card #2",
-    uri: "https://img.freepik.com/free-vector/abstract-blue-geometric-shapes-background_1035-17545.jpg?w=2000",
+    source: require("./assets/pic2.jpg"),
   },
   {
     id: 3,
     text: "Card #3",
-    uri: "http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg",
+    source: require("./assets/pic3.jpg"),
   },
   {
     id: 4,
     text: "Card #4",
-    uri: "http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg",
+    source: require("./assets/pic4.jpg"),
   },
   {
     id: 5,
     text: "Card #5",
-    uri: "http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg",
-  },
-  {
-    id: 6,
-    text: "Card #6",
-    uri: "http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg",
-  },
-  {
-    id: 7,
-    text: "Card #7",
-    uri: "http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg",
-  },
-  {
-    id: 8,
-    text: "Card #8",
-    uri: "http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg",
+    source: require("./assets/pic5.jpg"),
   },
 ];
 
 import { Card, Button } from "react-native-elements";
 export default function App() {
   const renderCard = (item) => {
+    console.log("item text ", item.text);
     return (
-      <Card key={item.id} title={item.text} image={{ uri: item.uri }}>
-        <Card.Image
-          style={{ padding: 0 }}
-          source={{
-            uri: item.uri,
-          }}
-        ></Card.Image>
+      <Card key={item.id}>
+        <Card.Image style={{ padding: 0 }} source={item.source}></Card.Image>
+
         <Card.Title>{item.text}</Card.Title>
         <Text>I can customized the Card further.</Text>
         <Button
@@ -66,18 +48,24 @@ export default function App() {
       </Card>
     );
   };
-
+  const renderNoMoreCards = () => {
+    return (
+      <Card>
+        <Card.Title>No more cards</Card.Title>
+        <Text>There is no more content here!</Text>
+      </Card>
+    );
+  };
   return (
     <View style={styles.container}>
       {/* <Ball /> */}
-      <Deck data={DATA} renderCard={renderCard}></Deck>
-      <Image
-        style={{ height: 30, width: 100 }}
-        source={{
-          uri: "https://reactnative.dev/img/tiny_logo.png",
-        }}
-      ></Image>
-      <Text>abc</Text>
+      <Deck
+        onSwipeRight={() => console.log("something was swiped")}
+        data={DATA}
+        renderCard={renderCard}
+        renderNoMoreCards={renderNoMoreCards}
+      ></Deck>
+
       <StatusBar style="auto" />
     </View>
   );
--- src/Deck.js
+++ src/Deck.js
@@ -5,13 +5,27 @@ import {
   Text,
   View,
   PanResponder,
+  LayoutAnimation,
+  UIManager,
+  Platform,
 } from "react-native";
 import React from "react";
 const SCREEN_WIDTH = Dimensions.get("window").width;
 const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
 const SWIPE_OUT_DURATION = 250;
-const Deck = ({ data, renderCard }) => {
+const Deck = ({
+  data,
+  renderNoMoreCards,
+  renderCard,
+  onSwipeRight,
+  onSwipeLeft,
+}) => {
   let [position, setPosition] = React.useState(new Animated.ValueXY());
+  let [index, setIndex] = React.useState(0);
+  React.useMemo(() => {
+    console.log("data changed");
+    setIndex(0);
+  }, [data]);
   let [panResponder, setPanResponder] = React.useState(
     PanResponder.create({
       onStartShouldSetPanResponder: () => true,
@@ -20,21 +34,24 @@ const Deck = ({ data, renderCard }) => {
           x: gesture.dx,
           y: gesture.dy,
         });
-        console.log("gesture ", gesture);
       },
       onPanResponderRelease: (event, gesture) => {
         if (gesture.dx > SWIPE_THRESHOLD) {
-          console.log("swipe right");
           forceSwipe("right");
         } else if (gesture.dx < -SWIPE_THRESHOLD) {
           forceSwipe("left");
-          console.log("swipe left");
         } else {
           resetPosition();
         }
       },
     })
   );
+  if (Platform.OS === "android") {
+    if (UIManager.setLayoutAnimationEnabledExperimental) {
+      UIManager.setLayoutAnimationEnabledExperimental(true);
+    }
+  }
+  LayoutAnimation.spring();
   const resetPosition = () => {
     Animated.spring(position, {
       toValue: {
@@ -44,7 +61,7 @@ const Deck = ({ data, renderCard }) => {
       useNativeDriver: false, // Add This line
     }).start();
   };
-  const forceSwipe = (direction, onSwipeRight, onSwipeLeft) => {
+  const forceSwipe = (direction) => {
     const x = direction === "right" ? SCREEN_WIDTH : -SCREEN_WIDTH;
     Animated.timing(position, {
       toValue: {
@@ -56,7 +73,11 @@ const Deck = ({ data, renderCard }) => {
     }).start(() => onSwipeComplete(direction));
   };
   const onSwipeComplete = (direction) => {
-    direction === "right" ? onSwipeRight() : onSwipeLeft();
+    console.log("index here", index);
+    const item = data[index];
+    direction === "right" ? onSwipeRight(item) : onSwipeLeft(item);
+    position.setValue({ x: 0, y: 0 });
+    setIndex((index) => index + 1);
   };
   const getCardStyle = () => {
     const rotate = position.x.interpolate({
@@ -65,26 +86,51 @@ const Deck = ({ data, renderCard }) => {
     });
     return { ...position.getLayout(), transform: [{ rotate: rotate }] };
   };
-  return (
-    <View>
-      {data.map((item, i) => {
-        if (i === 0) {
-          return (
-            <Animated.View
-              key={item.id}
-              style={getCardStyle()}
-              {...panResponder.panHandlers}
-            >
-              {renderCard(item)}
-            </Animated.View>
-          );
-        }
-        return renderCard(item);
-      })}
-    </View>
-  );
-};
+  if (index >= data.length) {
+    return renderNoMoreCards();
+  }
+  const x = data
+    .map((item, i) => {
+      if (i < index) {
+        return null;
+      }
+      if (i === index) {
+        console.log("index now", i, index, item.text);
+        return (
+          <Animated.View
+            key={item.id}
+            style={[getCardStyle(), styles.cardStyle, { zIndex: 5 }]}
+            {...panResponder.panHandlers}
+          >
+            {renderCard(item)}
+          </Animated.View>
+        );
+      }
+      return (
+        <Animated.View
+          key={item.id}
+          style={[styles.cardStyle, { top: 10 * (i - index) }]}
+        >
+          {renderCard(item)}
+        </Animated.View>
+      );
+    })
+    .reverse();
 
+  return <View>{x}</View>;
+};
+Deck.defaultProps = {
+  onSwipeRight: () => {},
+  onSwipeLeft: () => {},
+};
 export default Deck;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  cardStyle: {
+    position: "absolute",
+    width: SCREEN_WIDTH,
+    zIndex: 0,
+    // left: 0,
+    // right: 0,
+  },
+});
